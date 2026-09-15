868cd27cadd5d3bbe3e88a8ea423f9e899ebcde6
venkateshthooti <venkatesh.bi2108@gmail.com>
2026-09-15 14:56:23 +0530

    variables and data binding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Variables } from './components/variables/variables';
+import { DataBinding } from './components/data-binding/data-binding';
+import { Directives } from './components/directives/directives';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'variables',
+        component: Variables
+    },
+    {
+        path: 'data-binding',
+        component: DataBinding
+    },
+    {
+        path: 'directives',
+        component: Directives
+    },
+    {
+        path: '',
+        redirectTo: 'data-binding',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'data-binding',
+        pathMatch: 'full'
+    }
+];
---
+++ src/app/components/directives/directives.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  imports: [],
+  templateUrl: './directives.html',
+  styleUrl: './directives.scss',
+})
+export class Directives {}
