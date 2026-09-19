166a67afbd6bd884b89ae0a9a09bcd383c0bb414
venkateshthooti <venkatesh.bi2108@gmail.com>
2026-09-20 05:09:03 +0530

    Signal form and it's validation code added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { Logo } from './components/logo/logo';
 import { TemplateForm } from './components/template-form/template-form';
 import { ReactiveForm } from './components/reactive-form/reactive-form';
 import { SignalBasic } from './components/signal-basic/signal-basic';
+import { SignalForm } from './components/signal-form/signal-form';
 
 export const routes: Routes = [
     {
@@ -40,8 +41,12 @@ export const routes: Routes = [
 
     },
     {
-        path:'signal-form',
+        path:'signal-basic',
         component:SignalBasic
+    },
+    {
+        path:'signal-form',
+        component:SignalForm
     },
     {
         path: '',
